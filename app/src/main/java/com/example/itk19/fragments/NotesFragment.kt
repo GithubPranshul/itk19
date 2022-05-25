302642172127bc99fbdbfb0567efab84a47ff7dd
GithubPranshul <pranshulsingh9060@gmail.com>
2022-05-25 12:26:08 +0530

    uploading process done with Rdb

--- app/src/main/java/com/example/itk19/fragments/NotesFragment.kt
+++ app/src/main/java/com/example/itk19/fragments/NotesFragment.kt
@@ -1,60 +1,56 @@
 package com.example.itk19.fragments
 
+/*
+    There are some changes I would like to do
+    1) Firestore db instead of RealTime db
+    2) Deprecated things - startActivityForResult and ProgressDialog  (status - done)
+ */
 
-
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.activity.result.ActivityResultCallback
+import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 import com.example.itk19.R
 import com.example.itk19.models.FileInfoModel
-import com.google.android.gms.auth.api.signin.internal.Storage
-import com.google.firebase.database.ktx.database
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.ktx.storage
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
 import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
-import javax.security.auth.Subject
 
 
 class NotesFragment : Fragment() {
 
-    private lateinit var fileBrowse:ImageView
+    private lateinit var fileBrowse: ImageView
     private lateinit var fileLogo: ImageView
-    private lateinit var cancelFile:ImageView
+    private lateinit var cancelFile: ImageView
     private lateinit var uploadFile: Button
-    private lateinit var fileSubject:EditText
+    private lateinit var fileSubject: EditText
     private lateinit var fileSemester: EditText
+    private lateinit var progressBar:ProgressBar
 
-   val storageReference = Firebase.storage
-   val databaseReference = Firebase.firestore
+    private val storageReference = FirebaseStorage.getInstance().reference
+    private val databaseReference = FirebaseDatabase.getInstance().getReference("documents")
 
+    private lateinit var filePath: Uri
 
-    private lateinit var  filePath:Uri
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_notes, container, false)
 
@@ -66,83 +62,112 @@ class NotesFragment : Fragment() {
         fileLogo = view.findViewById(R.id.filelogo)
         cancelFile = view.findViewById(R.id.cancel_file)
         fileBrowse = view.findViewById(R.id.file_browse)
-        uploadFile = view.findViewById(R.id.image_upload)
+        uploadFile = view.findViewById(R.id.file_upload)
+        progressBar = view.findViewById(R.id.determinateBar)
 
 
 
-        fileLogo.visibility = View.GONE
-        cancelFile.visibility =View.GONE
+        fileLogo.visibility = View.INVISIBLE
+        cancelFile.visibility = View.INVISIBLE
         fileBrowse.visibility = View.VISIBLE
+
         cancelFile.setOnClickListener {
-            fileLogo.visibility = View.GONE
-            cancelFile.visibility = View.GONE
+            fileLogo.visibility = View.INVISIBLE
+            cancelFile.visibility = View.INVISIBLE
             fileBrowse.visibility = View.VISIBLE
-            Log.i("Clicked","cancelFile Clicked!..")
+            progressBar.visibility = View.INVISIBLE
+            fileSemester.setText("")
+            fileSubject.setText("")
+            Log.i("Clicked", "cancelFile Clicked!..")
         }
 
         fileBrowse.setOnClickListener {
-            Dexter.withContext(requireContext())
+
+                Dexter.withContext(context?.applicationContext)
                 .withPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                .withListener(object : PermissionListener{
-                    override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
-                        fileBrowse.setOnClickListener{
-                            takePdf.launch("application/pdf")
+                    .withListener(object : PermissionListener{
+                        override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                            val intent = Intent()
+                            intent.type = "application/pdf"
+                            intent.action = Intent.ACTION_GET_CONTENT
+                           // startActivityForResult(Intent.createChooser(intent,"select pdf files"),101)
+                            getResult.launch(intent)
                         }
 
+                        override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                            TODO("Not yet implemented")
+                        }
 
-                    }
+                        override fun onPermissionRationaleShouldBeShown(
+                            p0: PermissionRequest?,
+                            p1: PermissionToken?
+                        ) {
+                            p1?.continuePermissionRequest()
+                        }
 
-                    override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
-                        TODO("Not yet implemented")
-                    }
+                    }).check()
 
-                    override fun onPermissionRationaleShouldBeShown(
-                        p0: PermissionRequest?,
-                        p1: PermissionToken?
-                    ) {
-                        p1?.continuePermissionRequest()
-                    }
-
-                }).check()
         }
 
 
         // Code to Upload file
 
-         uploadFile.setOnClickListener {
+        uploadFile.setOnClickListener {
             processUpload(filePath)
-         }
+        }
 
         return view
 
     }
 
-    private fun processUpload(filePath: Uri) {
-        var storageRef:StorageReference? = storageReference.reference.child("uploads/"+System.currentTimeMillis()+".pdf")
-        if (storageRef != null) {
-            storageRef.putFile(filePath)
-                .addOnSuccessListener {
-                        storageRef.downloadUrl.addOnSuccessListener {
-                            FileInfoModel(fileSubject.text.toString(),fileSubject.text.toString(),filePath.toString())
-
-                            databaseReference.collection(databaseReference.push())
-                        }
-                }
-        }
 
 
-    }
-
-    val takePdf = registerForActivityResult(ActivityResultContracts.GetContent(),
-        ActivityResultCallback {
-            fileBrowse.setImageURI(it)
+    private val getResult = registerForActivityResult(
+    ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == RESULT_OK){
+            filePath = it.data?.data!!
             fileLogo.visibility = View.VISIBLE
             cancelFile.visibility = View.VISIBLE
-          //  fileBrowse.visibility = View.GONE
-        })
+            fileBrowse.visibility = View.INVISIBLE
+            progressBar.visibility = View.VISIBLE
+        }
+}
 
 
 
+    private fun processUpload(filePath: Uri) {
+
+
+
+    progressBar.visibility = View.VISIBLE
+
+            val storageRef = storageReference.child("uploads/"+System.currentTimeMillis()+"pdf")
+            storageRef.putFile(filePath)
+                .addOnSuccessListener {
+                    storageRef.downloadUrl.addOnSuccessListener {
+                        val obj = FileInfoModel(fileSemester.text.toString(),fileSubject.text.toString(),filePath.toString())
+                        databaseReference.push().key?.let { it1 -> databaseReference.child(it1).setValue(obj) }
+
+                        progressBar.visibility = View.INVISIBLE
+//                        pd.dismiss()
+                        Toast.makeText(context?.applicationContext,"File Uploaded",Toast.LENGTH_LONG).show()
+
+                        fileLogo.visibility = View.INVISIBLE
+                        cancelFile.visibility = View.INVISIBLE
+                        fileBrowse.visibility = View.VISIBLE
+                        fileSemester.setText("")
+                        fileSubject.setText("")
+
+                    }
+
+
+                }
+                .addOnProgressListener {
+                    val percent: Float = ((100)*it.bytesTransferred/it.totalByteCount).toFloat()
+                    //pd.setMessage("uploaded:"+percent.toInt()+"%")
+                    progressBar.progress = percent.toInt()
+                }
+    }
 
 
 }
